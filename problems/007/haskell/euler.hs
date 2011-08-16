bf1cb06129c11b75e7503eb9e496ccba6f6df8e2
Reid Draper <reiddraper@gmail.com>
2011-08-15 23:32:05 -0400

    solve problem 7 in haskell

---
+++ problems/007/haskell/euler.hs
@@ -0,0 +1,16 @@
+isPrime :: (Integral a) => a -> Bool
+isPrime 2 = True
+isPrime 3 = True
+isPrime x
+    | even x = False
+    | and  y = True
+    | otherwise = False
+    where y = map (\y -> x `mod` y /= 0) [3.. ceiling $ sqrt $ fromIntegral x]
+
+primes :: (Integral a) => [a]
+primes = 2 : 3 : filter isPrime [5,7..]
+
+
+main :: IO ()
+main = do
+    print $ primes !! 10000
